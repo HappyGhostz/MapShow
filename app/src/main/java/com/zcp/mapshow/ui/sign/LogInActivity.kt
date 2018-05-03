d589c9cd6a7a1f4a66b86f5841385d5426fed66b
赵小白 <zcphappyghost@163.com>
2018-05-03 22:24:18 +0800

    log in gif tab

--- app/src/main/java/com/zcp/mapshow/ui/sign/LogInActivity.kt
+++ app/src/main/java/com/zcp/mapshow/ui/sign/LogInActivity.kt
@@ -1,10 +1,10 @@
 package com.zcp.mapshow.ui.sign
 
-import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.drawee.view.SimpleDraweeView
+import android.graphics.drawable.AnimationDrawable
 import com.zcp.mapshow.R
 import com.zcp.mapshow.ui.base.BaseActivity
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.backgroundDrawable
 
 /**
  * @author Zhao Chenping
@@ -16,12 +16,19 @@ class LogInActivity:BaseActivity() {
     override fun getContentView(): Int  = R.layout.activity_login
 
     override fun initview() {
-        val tabView = find<SimpleDraweeView>(R.id.tab)
-        val builder = Fresco.newDraweeControllerBuilder()
-                .setUri("res:// /" + R.mipmap.tab)
-                .setAutoPlayAnimations(true)
-                .build()
-        tabView.controller = builder
+        //看到一个别人实现帧动画的思路，就是将所有的图片放到arrays文件里，动态获取，然后根据
+        //设置的播放时间，是否循环等条件动态设置imageView的背景，这里用到了异步线程，要考虑到内存泄漏的问题
+        //https://github.com/ansen360/FrameAnimation
+        val drawable = resources.getDrawable(R.drawable.login_tab_animation_list) as AnimationDrawable
+        tab.backgroundDrawable = drawable
+        drawable.start()
+        //在这里也可以用fresco加载gif，但我需要的是透明的 ，找不到透明gif就只好用上面的方法了
+//        val tabView = find<SimpleDraweeView>(R.id.tab)
+//        val builder = Fresco.newDraweeControllerBuilder()
+//                .setUri("res:// /" + R.mipmap.log_tab)
+//                .setAutoPlayAnimations(true)
+//                .build()
+//        tabView.controller = builder
     }
 
     override fun initData() {
